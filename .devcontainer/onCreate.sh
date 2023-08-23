e3f3f0a97722eac8ac753369035a7b0c3a227322
Josh Spicer <josh@joshspicer.com>
2023-08-23 20:09:17 +0000

    add examples and testing docker-in-docker

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -6,3 +6,7 @@ cd /tmp
 wget https://github.com/bradleyjkemp/grpc-tools/releases/download/v0.2.6/grpc-tools_0.2.6_Linux_amd64.zip -O /tmp/grpc-tools.zip
 unzip -o /tmp/grpc-tools.zip -d /tmp
 sudo mv /tmp/grpc* /usr/local/bin
+
+sudo apt update -y
+sudo apt install -y \
+    vim
