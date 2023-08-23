01b57fd5631397801784eda14ec665b50f63c15b
Josh Spicer <josh@joshspicer.com>
2023-08-23 19:14:33 +0000

    additional

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -4,5 +4,5 @@ npm install -g @devcontainers/cli
 
 cd /tmp
 wget https://github.com/bradleyjkemp/grpc-tools/releases/download/v0.2.6/grpc-tools_0.2.6_Linux_amd64.zip -O /tmp/grpc-tools.zip
-unzip /tmp/grpc-tools.zip -d /tmp -o
+unzip -o /tmp/grpc-tools.zip -d /tmp
 sudo mv /tmp/grpc* /usr/local/bin
