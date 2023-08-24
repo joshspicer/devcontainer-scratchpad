63a190ea966cc813af488ff45f37ba2d61fd9ddd
Josh Spicer <josh@joshspicer.com>
2023-08-24 17:16:38 +0000

    remove remote if already present

--- .devcontainer/setup-remote-builder.sh
+++ .devcontainer/setup-remote-builder.sh
@@ -4,6 +4,9 @@ set -e
 
 docker run -d --name buildkitd --privileged moby/buildkit:master
 export BUILDKIT_HOST=docker-container://buildkitd
+
+# Recreate builder instance
+docker buildx rm remote || true
 docker buildx create --name=remote --driver=remote
 docker buildx use remote
 
