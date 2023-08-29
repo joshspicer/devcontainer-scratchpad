3ae7315e1e3f3c7eab42183f56811bd373c60b2e
Josh Spicer <josh@joshspicer.com>
2023-08-29 11:50:18 -0700

    update repo name

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ fi
 set -e
 
 # https://docs.docker.com/build/building/env-vars/#buildx_config
-export EXPERIMENTAL_BUILDKIT_SOURCE_POLICY=/workspaces/devcontainer-scratchpad/policy.json
+export EXPERIMENTAL_BUILDKIT_SOURCE_POLICY=/workspaces/play/policy.json
 
-devcontainer build --workspace-folder /workspaces/devcontainer-scratchpad/example_configs/$CONFIG
+devcontainer build --workspace-folder /workspaces/play/example_configs/$CONFIG
